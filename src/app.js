57d12379266cf0288865ed209c2188f7c2b75c6b
RobinJessicaLittle <robin.jessica.little@outlook.com>
2022-04-27 15:35:38 +0100

    all CRUD + Stretch Goal met

--- src/app.js
+++ src/app.js
@@ -2,24 +2,38 @@ require("dotenv").config();
 const yargs = require("yargs");
 
 const {connections, client} = require ('./dataBase/connections')
-const {addMovie, listMovie, updateMovie, deleteMovie, findMovie} = require('./utils/index.js')
+const {addMovie, listMovie, updateMovie, deleteMovie, deleteAll, findMovie, updateActor, updateGenre } = require('./utils/index.js')
 
 const app = async (yargsObj) => {
     const collection = await connections();
+    //add movie
     if (yargsObj.add) {
-        await addMovie(collection, {title: yargsObj.title, actor: yargsObj.actor});
+        await addMovie(collection, {title: yargsObj.title, actor: yargsObj.actor, genre: yargsObj.genre});
         console.log('successfully added new doc too database')
+    //list movies in database
     }else if (yargsObj.list){
         await listMovie(collection)
-    }else if (yargsObj.update){
-        await updateMovie(collection, 
-        {title: yargsObj.title},
-        { $set: {
-            actor:yargsObj.actor,
-        },
-    });
+    //update title, actor or genre
+    }else if (yargsObj.update) {
+        await updateMovie(collection, {title: yargsObj.title}, yargsObj.newTitle);
+        await updateActor(collection, {actor: yargsObj.actor}, yargsObj.newActor);
+        await updateGenre(collection, {genre: yargsObj.genre}, yargsObj.newGenre);
+    //delete selected movie entry from database
     }else if (yargsObj.delete){
         await deleteMovie(collection, {title: yargsObj.title})
+    //Delete all database entries- doesn't work
+    /*}else if (yargsObj.deleteAll){
+        await deleteAll(collection)*/
+    //find Movie via title
+    }
+    else if (yargsObj.findTitle) {
+        await findMovie(collection, { title: yargsObj.title });
+    //find Movie via Actor
+    }else if (yargsObj.findActor) {
+        await findMovie(collection, { actor: yargsObj.actor });
+    //find Movie via Genre
+    }else if (yargsObj.findGenre) {
+        await findMovie(collection, { genre: yargsObj.genre });
     }else {
         console.log ('incorrect command')
     }
@@ -28,6 +42,13 @@ const app = async (yargsObj) => {
 
 app(yargs.argv)
 
-//node src/app.js --add --title='Death Proof' --actor='Mary Elizabeth Winstead' WORKING
+//node src/app.js --add --title='Death Proof' --actor='Mary Elizabeth Winstead' --genre='Action' WORKING
 //node src/app.js --list WORKING
-//node src/app.js --update
+//node src/app.js --update --title="But Im a Cheerleader" --newTitle="But I am a Chearleader" WORKING
+//node src/app.js --update --actor="Mary Elizabeth Winstead" --newActor="Rosario Dawson" WORKING
+//node src/app.js --update --genre="Action" --newGenre="Action&Adventure" WORKING
+//node src/app.js --delete --title='Death Proof' --actor='Mary Elizabeth Winstead' WORKING
+//node src/app.js --findTitle --title='Devil Wears Prada' WORKING
+//node src/app.js --findActor --actor='Anne Hathaway' WORKING
+//node src/app.js --findGenre --genre='Action&Adventure' WORKING
+//node src/app.js --deleteAll NOT WORKING
--- src/utils/index.js
+++ src/utils/index.js
@@ -22,22 +22,54 @@ exports.findMovie=async(collection,movieObj)=>{
     try{
         const movieList=await collection.find(movieObj).toArray();
         console.log(movieList);
-    }catch (err) {console.log(err);}
-}
-exports.updateMovie = async (collection, movieObj, updateObj) => {
-    try{
-        const updateEntry = await collection.updateOne(movieObj, updateObj);
-        console.log(updateEntry);
-    } catch (error) {
-        console.log(error)
+    }catch (error) {
+        console.log("Failed to find")
     }
-};
+}
+exports.updateMovie = async (collection, title, newTitle) => {
+    try{
+        console.log("Movie Updated")
+        let updateList = await collection.updateOne(title, {$set : {title: newTitle}} )
+        console.log(updateList)
+    } catch (error) {
+        console.log("updateMovies fail")
+    }
+}
+
+exports.updateActor = async (collection, actor, newActor) => {
+    try{
+        console.log("Actor Updated")
+        let updateActor = await collection.updateOne(actor, {$set : {actor: newActor}} )
+        console.log(updateActor)
+    } catch (error) {
+        console.log("updateActor fail")
+    }
+}
+
+exports.updateGenre = async (collection, genre, newGenre) => {
+    try{
+        console.log("Genre Updated")
+        let updateGenre = await collection.updateOne(genre, {$set : {genre: newGenre}} )
+        console.log(updateGenre)
+    } catch (error) {
+        console.log("updateGenre fail")
+    }
+}
 
 exports.deleteMovie = async (collection, movieObj) => {
     try{
         const deleteEntry = await collection.deleteOne(movieObj);
         console.log(deleteEntry)
     }catch (error) {
-        console.log(error)
+        console.log("Failed too delete")
+    }
+};
+
+exports.deleteAll = async (collection) => {
+    try{
+        const deleteAll = await collection.deleteMany(movieObj);
+        console.log(deleteAll)
+    }catch (error) {
+        console.log("Failed too delete")
     }
 };
